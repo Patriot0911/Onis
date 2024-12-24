04384e3b283b68d7e2d566c79cb0e07644a5e6c8
oles.potaskalov <alexandr.potaskalov@gmail.com>
2024-12-24 12:18:03 +0200

    added simple frontend test & workflow jobs for testing

---
+++ packages/web/src/__tests__/components/AuthForm/index.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import AuthForm from '@/components/AuthForm'
+import '@testing-library/jest-dom'
+
+jest.mock('next/navigation', () => ({
+    useRouter: () => ({
+        push: jest.fn(),
+    }),
+    usePathname: jest.fn(),
+    useSearchParams: jest.fn().mockReturnValue({
+        get: jest.fn().mockReturnValue('true'),
+    }),
+}));
+
+jest.mock('react-redux', () => ({
+    useDispatch: jest.fn(),
+}));
+
+describe('Auth form Tests', () => {
+    it('the submit button must be present', () => {
+        render(<AuthForm />)
+
+        const sumbitButton = screen.getByTestId('auth-form-submit-button');
+
+        expect(sumbitButton).toBeInTheDocument()
+    })
+})
