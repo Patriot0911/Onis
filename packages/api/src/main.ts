0dd27ac308255e0b1d87e099e3a8a1957e8db7d0
oles.potaskalov <alexandr.potaskalov@gmail.com>
2024-12-24 04:24:47 +0200

    added env for cors url param

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Потім поставити домен або з env
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
